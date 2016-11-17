f4c75dfcd6ae2d3a1dd1b302672054ee8aa7fab9
Daniel Gillies <danny@skully.com>
2016-11-16 16:17:11 -0800

    trying to fix resizing of header video

--- Forecast/WRV/assets/js/index.js
+++ Forecast/WRV/assets/js/index.js
@@ -1,4 +1,15 @@
 jQuery(document).ready(function($) {
+
+    // Resive video
+    scaleVideoContainer();
+
+    initBannerVideoSize('.video-container video');
+
+    $(window).on('resize', function() {
+        scaleVideoContainer();
+        scaleBannerVideoSize('.video-container video');
+    });
+
     var hash = window.location.hash;
     $(hash).show();
 
@@ -49,3 +60,60 @@ jQuery(document).ready(function($) {
         $("#home").show();
     })
 })
+
+function scaleVideoContainer() {
+
+    var height = $(window).height();
+    var unitHeight = parseInt(height) + 'px';
+    $('.video-background').css('height', unitHeight);
+
+}
+
+function initBannerVideoSize(element) {
+
+    $(element).each(function() {
+        $(this).data('height', $(this).height());
+        $(this).data('width', $(this).width());
+    });
+
+    scaleBannerVideoSize(element);
+
+}
+
+function scaleBannerVideoSize(element) {
+
+    var windowWidth = $(window).width(),
+        windowHeight = $(window).height(),
+        videoWidth,
+        videoHeight;
+
+    $(element).each(function() {
+        var videoAspectRatio = $(this).data('height') / $(this).data('width'),
+            windowAspectRatio = windowHeight / windowWidth;
+
+        if (videoAspectRatio > windowAspectRatio) {
+            videoWidth = windowWidth;
+            videoHeight = videoWidth * videoAspectRatio;
+            console.log(windowWidth);
+            console.log(videoWidth);
+            $(this).css({
+                'top': -(videoHeight - windowHeight) / 2 + 'px',
+                'margin-left': 0
+            });
+        } else {
+            videoHeight = windowHeight;
+            videoWidth = videoHeight / videoAspectRatio;
+            $(this).css({
+                'margin-top': 0,
+                'margin-left': -(videoWidth - windowWidth) / 2 + 'px'
+            });
+        }
+
+        $(this).width(videoWidth).height(videoHeight);
+
+        $('.video-background .video-container video').addClass('fadeIn animated');
+
+
+    });
+}
+
